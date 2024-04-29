b9f9cee2f62e260922c2b570c162d5c29e43f653
Daniel Martins <martinsda@gmail.com>
2024-04-29 19:35:38 -0300

    add Where section

--- src/app/components/Where.tsx
+++ src/app/components/Where.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Image from "next/image";
+
+import ExchangeBox from "../assets/Exchange-box.png";
 
 const Where = () => {
   return (
-    <div className="relative w-full h-[1100px] flex justify-center bg-gradient-to-t from-dark-green to-light-green">
-      <div className="absolute flex justify-between w-[70%] mt-40">Where</div>
+    <div className="w-full h-[1100px] flex items-center justify-center bg-gradient-to-t from-dark-green to-light-green">
+      <div className="flex flex-col w-[594px] items-center gap-y-12 text-center text-white">
+        <h2 className="text-7xl">
+          Send from the
+          <br />
+          US to LATAM
+        </h2>
+        <h5 className="text-3xl">Find the full list here</h5>
+        <Image src={ExchangeBox} alt="Exchange box" />
+      </div>
     </div>
   );
 };
