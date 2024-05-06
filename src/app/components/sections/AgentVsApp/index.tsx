1d98ed1e21bd905cac40a8cde6dcd507de374cce
Daniel Martins <martinsda@gmail.com>
2024-05-05 23:07:28 -0300

    edit Agent vs App section styling

--- src/app/components/sections/AgentVsApp/index.tsx
+++ src/app/components/sections/AgentVsApp/index.tsx
@@ -2,40 +2,72 @@ import { FC } from "react";
 import Image from "next/image";
 
 import Button from "../../ui/Button";
-import AgentModel from "../../../assets/Agent-model.png";
-import MobileApp from "../../../assets/Mobile-App.svg";
+import App1 from "../../../assets/delivered.png";
+import App2 from "../../../assets/map.png";
+import App3 from "../../../assets/new-transaction.png";
+import App4 from "../../../assets/overview.png";
+import App5 from "../../../assets/my-account.png";
+import AppStore from "../../../assets/app-store.svg";
+import GooglePlay from "../../../assets/google-play.svg";
+import ConvenienceStore from "../../../assets/convenience-store-3.jpeg";
 
 const AgentVsApp: FC = () => {
   return (
-    <div className="relative w-full h-[1100px] flex justify-center bg-really-light-green">
-      <div className="absolute flex justify-between w-[70%] mt-40">
-        <Image src={AgentModel} alt="Agent Model" />
-        <div className="flex flex-col items-center justify-center w-[589px] text-center">
-          <h2 className="text-green text-[52px] leading-[60px] font-bold">
-            Licensed Agent Stores
-          </h2>
-          <h5 className="text-green font-sans font-normal text-[27px] mt-8">
-            Ideally located on your trusted convenience store, all the benefits
-            of sending with LibertyPay at your everyday store!
-          </h5>
-          <Button href="#">Add LibertyPay to my store</Button>
+    <div className="w-full h-screen flex justify-center bg-white">
+      <div className="w-[75%] h-[80%] flex items-center justify-around gap-10">
+        <div className="w-[42.5%] h-[800px] flex flex-col justify-between p-14 bg-really-light-green rounded-[50px]">
+          <div className="flex flex-col flex-1 gap-4">
+            <h2 className="text-green text-3xl font-bold">
+              Licensed Agent Stores
+            </h2>
+            <h5 className="text-green font-sans font-normal text-[22px]">
+              Ideally located on your trusted convenience store, all the
+              benefits of sending with LibertyPay at your everyday store.
+            </h5>
+            <div className="mx-auto">
+              <Image
+                className="rounded-3xl object-scale-down"
+                src={ConvenienceStore}
+                // src={AgentModel}
+                alt="agents"
+              />
+            </div>
+          </div>
+          <div className="w-fit">
+            <Button href="https://libertypay.activehosted.com/f/1">
+              Add LibertyPay to my store
+            </Button>
+          </div>
         </div>
-      </div>
-      <Image
-        className="absolute right-[10%] bottom-0 w-[549px] h-[549px]"
-        src={MobileApp}
-        alt="Mobile app"
-      />
-      <div className="absolute bottom-28 left-[20%] mt-[35%]">
-        <div className="flex flex-col items-center justify-center w-[589px] text-center">
-          <h2 className="text-green text-[52px] leading-[60px] font-bold">
-            Mobile app
-          </h2>
-          <h5 className="text-green font-sans font-normal text-[27px] mt-8">
-            Send money home from the convenience of your own phone. Download the
-            LibertyPay App and start saving!
-          </h5>
-          <Button href="#">Download the LibertyPay App</Button>
+        <div className="w-[42.5%] h-[800px] flex flex-col justify-between p-14 bg-slate-50 rounded-[50px]">
+          <div className="flex items-center justify-center gap-3">
+            <Image className="w-[27.5%]" src={App3} alt="LibertyPay app" />
+            <Image className="w-[35%]" src={App2} alt="LibertyPay app" />
+            <Image className="w-[27.5%]" src={App1} alt="LibertyPay app" />
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col">
+              <h2 className="text-green text-3xl font-bold mt-8">Mobile app</h2>
+              <h5 className="text-green font-sans font-normal text-[22px] mt-4">
+                Send money home from the convenience of your own phone. Download
+                the LibertyPay App and start saving.
+              </h5>
+            </div>
+            <div className="flex gap-10 mt-4">
+              <a
+                className="hover:scale-110 hover:duration-500 hover:cursor-pointer"
+                href="https://apps.apple.com/us/app/libertypay-money-transfer/id6454528073"
+              >
+                <Image src={AppStore} alt="Download on the App Store" />
+              </a>
+              <a
+                className="hover:scale-110 hover:duration-500 hover:cursor-pointer"
+                href="https://play.google.com/store/apps/details?id=com.libertypay.b2c"
+              >
+                <Image src={GooglePlay} alt="Download on Google Play" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
