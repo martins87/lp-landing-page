8e559d85f3057a4853464eb9ec599e3a26ec3d83
Daniel Martins <martinsda@gmail.com>
2024-05-20 09:56:34 -0300

    edit Supported countries section responsiveness

--- src/components/sections/SupportedCountries/FlagBox.tsx
+++ src/components/sections/SupportedCountries/FlagBox.tsx
@@ -9,7 +9,7 @@ type FlagBoxProps = {
 const responsiveClasses = {
   default: "rounded-full w-16 h-16 min-w-16 drop-shadow-2xl",
   mobile: "mobile:w-20 mobile:h-20 mobile:min-w-20 mobile:min-h-20",
-  tablet: "tablet:w-28 tablet:h-28 tablet:min-w-28 tablet:min-h-28",
+  xl: "xl:w-28 xl:h-28 xl:min-w-28 xl:min-h-28",
 };
 
 const FlagBox: FC<FlagBoxProps> = ({ flag }) => {
--- src/components/sections/SupportedCountries/index.tsx
+++ src/components/sections/SupportedCountries/index.tsx
@@ -10,19 +10,19 @@ const Where: FC = () => {
   return (
     <Container className="">
       <div
-        className="w-[75%] tablet:w-[60%] flex flex-col xl:flex-row items-center justify-evenly gap-8 xl:gap-8 my-20 tablet:mt-16 tablet:mb-32 py-10 tablet:py-16 px-10 lg:px-20 xl:px-12 text-center tracking-wider bg-really-light-green rounded-[50px]"
+        className="w-[75%] tablet:w-full xl:w-[60%] flex flex-col tablet:flex-row items-center justify-evenly gap-8 xl:gap-8 mx-10 xl:mx-0 my-20 tablet:mt-16 tablet:mb-32 py-10 tablet:py-16 px-10 lg:px-20 xl:px-12 text-center tracking-wider bg-really-light-green rounded-[50px]"
         id="countries"
       >
-        <div className="xl:order-first flex gap-5 mobile:gap-8">
+        <div className="tablet:order-first flex gap-5 mobile:gap-6 xl:gap-8">
           <FlagBox flag={BrazilFlag} />
           <FlagBox flag={GuatemalaFlag} />
           <FlagBox flag={DominicanRepublicFlag} />
         </div>
-        <div className="order-first flex flex-col justify-between text-left gap-2 xl:w-[30%] 2xl:w-[40%]">
-          <h5 className="text-green-xl text-center xl:text-left text-2xl font-bold">
+        <div className="order-first flex flex-col justify-between text-left gap-2 tablet:w-[40%] xl:w-[30%] 2xl:w-[40%]">
+          <h5 className="text-green-xl text-center tablet:text-left text-2xl font-bold">
             Supported countries
           </h5>
-          <h5 className="text-green text-center xl:text-left text-lg">
+          <h5 className="text-green text-center tablet:text-left text-lg">
             LibertyPay currently allows you to send to friends and family in
             Brazil, Guatemala and Dominican Republic
           </h5>
